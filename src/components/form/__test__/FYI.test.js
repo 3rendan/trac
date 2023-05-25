ae65c92b5686ee96b9ec7598ce1fcc1d2cd7b60b
Brendan Ryan <brendan.ryan@pm.me>
2023-05-25 11:42:26 -0400

    Testing for successful form submission, disabled/ enabled Register button

--- src/components/form/__test__/FYI.test.js
+++ src/components/form/__test__/FYI.test.js
@@ -6,8 +6,3 @@ test('renders FYI text', () => {
   const fyiInfo = screen.getByText(/offer immediate TRAC report subscriptions/i)
   expect(fyiInfo).toBeInTheDocument()
 })
-test('renders FYI header', () => {
-  render( <FYI />)
-  const fyiHeader = screen.queryByText(/holly golightly/i)
-  expect(fyiHeader).toBeNull()
-})
--- src/components/form/__test__/SubscriptionForm.test.js
+++ src/components/form/__test__/SubscriptionForm.test.js
@@ -1,17 +1,59 @@
 import { render, fireEvent, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import SubscriptionForm from '../SubscriptionForm'
-import Terms from '../Terms'
 import { TracsContextProvider } from '../../../context/TracsContext'
 import { ProgramsProvider } from '../../../context/ProgramsContext'
+import SubscriptionForm from '../SubscriptionForm'
+import App from '../../../App'
+import { ToastContainer } from 'react-toastify'
 
+const submitForm = async() => {
+  const submitBtn = await screen.findByRole('button', {name: /register/i})
+  fireEvent.click(submitBtn)
+}
+
+const acceptTerms = () => {
+  const tou = screen.getByRole('checkbox')
+  fireEvent.click(tou)
+}
+
+const successToast = async () => {
+  const toastSuccess = await screen.findByRole('alert', {name: 'You have successfully submitted your carriage service request and will recieve a response in 2 to 3 days.'})
+  expect(toastSuccess).toBeInTheDocument()
+}
+
+const errorToast = async (e) => {
+  const toastError = await screen.findByRole('alert', {name: e})
+  expect(toastError).toBeInTheDocument()
+  
+}
+
+
+const blankForm = async () => {
+  const form = await screen.findByRole('form')
+  expect(form.value).toBe('')
+  
+}
+
+
+test('renders form properly', () => {
+  render(<TracsContextProvider><ProgramsProvider><SubscriptionForm /></ProgramsProvider></TracsContextProvider>)
+  expect(screen.getByPlaceholderText(/type to search/i)).toBeInTheDocument()
+  expect(screen.getByPlaceholderText(/Subscriber's Name/i)).toBeInTheDocument()
+  expect(screen.getByPlaceholderText(/Subscriber's Company/i)).toBeInTheDocument()
+  expect(screen.getByPlaceholderText(/Subscriber's title/i)).toBeInTheDocument()
+  expect(screen.getByPlaceholderText(/Subscriber's email/i)).toBeInTheDocument()
+  expect(screen.getByPlaceholderText(/Subscriber's phone number/i)).toBeInTheDocument()
+  expect(screen.getByPlaceholderText(/subscription period/i)).toBeInTheDocument()
+  expect(screen.getByPlaceholderText(/start date/i)).toBeInTheDocument()
+})
 
 test('terms button disabled',  () => {
   render(<TracsContextProvider><ProgramsProvider><SubscriptionForm /></ProgramsProvider></TracsContextProvider>)
   expect(screen.getByRole('button', {name: /register/i})).toBeDisabled()
 })
+
 test('if form is filled and terms accepted register button becomes enabled', async () => {
-  render(<TracsContextProvider><ProgramsProvider><SubscriptionForm /><Terms /></ProgramsProvider></TracsContextProvider>)
+  render(<TracsContextProvider><ProgramsProvider><SubscriptionForm /></ProgramsProvider></TracsContextProvider>)
   userEvent.type(screen.getByPlaceholderText(/type to search/i), 'React Testing')
   userEvent.type(screen.getByPlaceholderText(/Subscriber's Name/i), 'React Testing')
   userEvent.type(screen.getByPlaceholderText(/Subscriber's Company/i), 'React Testing')
@@ -20,41 +62,28 @@ test('if form is filled and terms accepted register button becomes enabled', asy
   userEvent.type(screen.getByPlaceholderText(/Subscriber's phone number/i), '401 555-5555')
   userEvent.type(screen.getByPlaceholderText(/subscription period/i), '1 Year')
   userEvent.type(screen.getByPlaceholderText(/start date/i), '05/23/2023')
-  const touCheck = screen.getByRole('checkbox')
-  fireEvent.click(touCheck)
+  acceptTerms()
   expect(await screen.findByRole('button', {name: /register/i})).toBeEnabled()
 })
 
-test('renders form properly', () => {
-  render(<TracsContextProvider><ProgramsProvider><SubscriptionForm /></ProgramsProvider></TracsContextProvider>)
-  const programField = screen.getByPlaceholderText(/type to search/i)
-  const nameField = screen.getByPlaceholderText(/Subscriber's Name/i)
-  const workplaceField = screen.getByPlaceholderText(/Subscriber's Company/i)
-  const jobField = screen.getByPlaceholderText(/Subscriber's title/i)
-  const emailField = screen.getByPlaceholderText(/Subscriber's email/i)
-  const phoneField = screen.getByPlaceholderText(/Subscriber's phone number/i)
-  const periodField = screen.getByPlaceholderText(/subscription period/i)
-  const startDateField = screen.getByPlaceholderText(/start date/i)
-  expect(programField).toBeInTheDocument()
-  expect(nameField).toBeInTheDocument()
-  expect(workplaceField).toBeInTheDocument()
-  expect(jobField).toBeInTheDocument()
-  expect(emailField).toBeInTheDocument()
-  expect(phoneField).toBeInTheDocument()
-  expect(periodField).toBeInTheDocument()
-  expect(startDateField).toBeInTheDocument()
+test('successful submission results in toastify success message, cleared form, and terms of use false', () => {
+  render(<TracsContextProvider><ProgramsProvider><App /><ToastContainer /><SubscriptionForm /></ProgramsProvider></TracsContextProvider>)
+  userEvent.type(screen.getByPlaceholderText(/type to search/i), 'React Testing')
+  userEvent.type(screen.getByPlaceholderText(/Subscriber's Name/i), 'React Testing')
+  userEvent.type(screen.getByPlaceholderText(/Subscriber's Company/i), 'React Testing')
+  userEvent.type(screen.getByPlaceholderText(/Subscriber's title/i), 'React Testing')
+  userEvent.type(screen.getByPlaceholderText(/Subscriber's email/i), 'thneed@pm.me')
+  userEvent.type(screen.getByPlaceholderText(/Subscriber's phone number/i), '401 555-5555')
+  userEvent.type(screen.getByPlaceholderText(/subscription period/i), '1 Year')
+  userEvent.type(screen.getByPlaceholderText(/start date/i), '05/23/2023')
+  acceptTerms()
+  submitForm()
+  successToast()
+  blankForm()
 })
-// test('form makes post with proper params', async () => {
-//   APIService.getD
-//   fireEvent.change(programField, { 'target': 'value': 'sample value'})
-//   fireEvent.change(nameField, { 'target': 'value': 'sample value'})
-//   fireEvent.change(workplaceField, { 'target': 'value': 'sample value'})
-//   fireEvent.change(jobField, { 'target': 'value': 'sample value'})
-//   fireEvent.change(emailField, { 'target': 'value': 'sample value'})
-//   fireEvent.change(phoneField, { 'target': 'value': '000 000-0000'})
-//   fireEvent.change(periodField, { 'target': 'value': '1 Year'})
-//   fireEvent.change(startDateField, { 'target': 'value': '05/23/23'})
-//   fireEvent.click(submitBtn)
 
+// Need to test that an error appears if the form does not submit successfully
+// test('error alert appears if form not submitted successfully', () => {
+
+//   errorToast()
 // })
-
